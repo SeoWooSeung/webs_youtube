cda5df7fda18b75312ffd46d5fe462d91a3c09a1
SeoWooSeung <dntmd1531@naver.com>
2024-05-13 20:24:59 +0900

    페이지SEO작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Main from "../components/section/Main";
 
 const Channel = () => {
-  return<Main>Channels</Main>
+  return (
+    <Main title="유튜브채널" description="유튜브 채널에 오신것을 환영합니다.">
+      Channels
+    </Main>
+  );
 };
 
 export default Channel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Main from "../components/section/Main";
 
 const Home = () => {
-  return <Main>Home</Main>;
+  return (
+    <Main title="유튜브채널" description="유튜브 채널에 오신것을 환영합니다.">
+      Home
+    </Main>
+  );
 };
 
 export default Home;
