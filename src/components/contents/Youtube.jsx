676626bb536434251450bee14e1595b15fda0111
SeoWooSeung <dntmd1531@naver.com>
2024-05-20 21:36:03 +0900

    메인 섹션 나머지 컨텐츠 작업

--- src/components/contents/Youtube.jsx
+++ src/components/contents/Youtube.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { youtubeText } from "../../data/youtube";
+import { Link } from "react-router-dom";
 
 const Youtube = () => {
-  return <div>Youtube</div>;
+  return (
+    <section id="youtube">
+      <h2>😮 gsap 패럴렉스 효과를 사용하고 싶다면 !</h2>
+      <div className="video__inner">
+        {youtubeText.map((video, key) => (
+          <div className="video" key={key}>
+            <div className="video__thumb play__icon">
+              <Link to={`/video/${video.videoId}`}>
+                <img src={video.img} alt={video.title} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Youtube;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,14 @@ import Website from "../components/contents/Website";
 import Gsap from "../components/contents/Gsap";
 import Port from "../components/contents/Port";
 import Youtube from "../components/contents/Youtube";
+import Webd from "../components/contents/Webd";
 
 const Home = () => {
   return (
     <Main title="유튜브채널" description="유튜브 채널에 오신것을 환영합니다.">
       <Today />
       <Developer />
+      <Webd />
       <Website />
       <Gsap />
       <Port />
